35f1dcf3a678864255e1cac46fa3ed0787f67b36
Thomas Delacour <thomas.delacour@protonmail.com>
2021-11-07 15:00:00 +0100

    Change authorization to use base64 encoded strings

--- api_models.py
+++ api_models.py
@@ -9,21 +9,23 @@ import datetime
 from enum import Enum
 import pickle
 import glob
+import base64
+import binascii
 
 api = FastAPI(
     title='API Models',
-    description=
-    """This API allow to get predict from trained Machine Learning models
+    description="""This API allow to get predict from trained
+     Machine Learning models
     """,
     version="1.0.0"
 )
 
 
 credentials = [
-  "alice:wonderland",
-  "bob:builder",
-  "clementine:mandarine",
-  "admin:4dm1N"
+    b"alice:wonderland",
+    b"bob:builder",
+    b"clementine:mandarine",
+    b"admin:4dm1N"
 ]
 
 
@@ -45,19 +47,40 @@ def index() -> dict:
 
     return {"status": "running"}
 
+
 @api.get('/info', name='Get informations')
-def get_info(authorization_header=Header(default="Basic ",
-                                  description="authorization header need to contain credentials of user (username:password)")) -> dict:
+def get_info(authorization_header=Header(
+    default="Basic ",
+    description="""authorization header need to contain
+    credentials of user (username:password)""")) -> dict:
     """
-    Return informations on API
+    Return informations on the API
     """
+    if "Basic " not in authorization_header:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid authorization header")
 
-    authorization_string = re.search("Basic \w*:\w*",authorization_header)
+    authorization_string = re.search("Basic .*", authorization_header)
 
-    if authorization_string is None or authorization_string.group().split("Basic ")[1] not in credentials:
-        raise HTTPException(status_code=403, detail="You do not have authorization to acces this ressource")
+    if authorization_string is None:
+        authorization_bytes = None
+    else:
+        authorization_bytes = authorization_string.group(
+        ).split('Basic')[1].strip().encode()
+
+    try:
+        authorization_decoded = base64.decodebytes(authorization_bytes)
+    except (binascii.Error, TypeError):
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid base64 string")
+
+    if (authorization_decoded is None
+            or authorization_decoded not in credentials):
+        raise HTTPException(
+            status_code=403,
+            detail="You do not have authorization to acces this ressource")
 
     # Return models list
     return {"models": list(models.keys())}
-
-    
