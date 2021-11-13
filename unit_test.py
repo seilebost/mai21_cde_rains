32f7e9981e93a092269c5b1b0bf297af0cfc770c
Thomas Delacour <thomas.delacour@protonmail.com>
2021-11-13 11:33:24 +0100

    Add first version for unit tests

---
+++ unit_test.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def test_api_status():
+    res = requests.get("http://127.0.0.1:8000")
+
+    assert res.status_code == 200
+    assert res.json() == {'status': 'running'}
+
+
+def test_api_info():
+    res = requests.get("http://127.0.0.1:8000/info",
+                       headers={'authorization-header':
+                                'Basic YWxpY2U6d29uZGVybGFuZA=='})
+
+    assert res.status_code == 200
