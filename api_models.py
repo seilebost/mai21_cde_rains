60791edb456f5f833fdaa6f0a4fa37ac5e1e95c4
Thomas Delacour <thomas.delacour@protonmail.com>
2021-11-13 11:35:58 +0100

    Reformat python files

--- api_models.py
+++ api_models.py
@@ -13,11 +13,11 @@ import base64
 import binascii
 
 api = FastAPI(
-    title='API Models',
+    title="API Models",
     description="""This API allow to get predictions from trained
      Machine Learning models
     """,
-    version="1.0.0"
+    version="1.0.0",
 )
 
 
@@ -25,7 +25,7 @@ credentials = [
     b"alice:wonderland",
     b"bob:builder",
     b"clementine:mandarine",
-    b"admin:4dm1N"
+    b"admin:4dm1N",
 ]
 
 
@@ -33,13 +33,13 @@ model_files = glob.glob("*_trained")
 models = {}
 
 for file_model in model_files:
-    with open(file_model, 'rb') as mod_file:
+    with open(file_model, "rb") as mod_file:
         model = pickle.load(mod_file)
 
     models[type(model).__name__] = model
 
 
-@api.get('/', name='Check API')
+@api.get("/", name="Check API")
 def index() -> dict:
     """
     Return a dictionnary that indicate the api is running
@@ -48,39 +48,44 @@ def index() -> dict:
     return {"status": "running"}
 
 
-@api.get('/info', name='Get informations')
-def get_info(authorization_header=Header(
-    default="Basic ",
-    description="""authorization header need to contain
-    credentials of user (username:password)""")) -> dict:
+@api.get("/info", name="Get informations")
+def get_info(
+    authorization_header=Header(
+        default="Basic ",
+        description="""authorization header need to contain
+    credentials of user (username:password)""",
+    )
+) -> dict:
     """
     Return informations on the API
     """
     if "Basic " not in authorization_header:
         raise HTTPException(
-            status_code=400,
-            detail="Invalid authorization header")
+            status_code=400, detail="Invalid authorization header"
+        )
 
     authorization_string = re.search("Basic .*", authorization_header)
 
     if authorization_string is None:
         authorization_bytes = None
     else:
-        authorization_bytes = authorization_string.group(
-        ).split('Basic')[1].strip().encode()
+        authorization_bytes = (
+            authorization_string.group().split("Basic")[1].strip().encode()
+        )
 
     try:
         authorization_decoded = base64.decodebytes(authorization_bytes)
     except (binascii.Error, TypeError):
-        raise HTTPException(
-            status_code=400,
-            detail="Invalid base64 string")
+        raise HTTPException(status_code=400, detail="Invalid base64 string")
 
-    if (authorization_decoded is None
-            or authorization_decoded not in credentials):
+    if (
+        authorization_decoded is None
+        or authorization_decoded not in credentials
+    ):
         raise HTTPException(
             status_code=403,
-            detail="You do not have authorization to acces this ressource")
+            detail="You do not have authorization to acces this ressource",
+        )
 
     # Return models list
     return {"models": list(models.keys())}
--- unit_test.py
+++ unit_test.py
@@ -5,12 +5,13 @@ def test_api_status():
     res = requests.get("http://127.0.0.1:8000")
 
     assert res.status_code == 200
-    assert res.json() == {'status': 'running'}
+    assert res.json() == {"status": "running"}
 
 
 def test_api_info():
-    res = requests.get("http://127.0.0.1:8000/info",
-                       headers={'authorization-header':
-                                'Basic YWxpY2U6d29uZGVybGFuZA=='})
+    res = requests.get(
+        "http://127.0.0.1:8000/info",
+        headers={"authorization-header": "Basic YWxpY2U6d29uZGVybGFuZA=="},
+    )
 
     assert res.status_code == 200
