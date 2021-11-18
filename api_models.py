4a695d58dfd2a5886d57cd2c044a3df1b5b8780f
Thomas Delacour <thomas.delacour@protonmail.com>
2021-11-18 19:20:45 +0100

    Add check_authorization for authentication with Depends inside endpoints; add get_data function to retrieve tests data; create classes to validate model name for endpoints; defined /score endpoints

--- api_models.py
+++ api_models.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Header, Request
+from fastapi import FastAPI, HTTPException, Header, Request, Depends
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Optional, List
@@ -11,6 +11,9 @@ import pickle
 import glob
 import base64
 import binascii
+import uvicorn
+
+from project1_train import get_data
 
 api = FastAPI(
     title="API Models",
@@ -27,6 +30,38 @@ credentials = [
     b"clementine:mandarine",
     b"admin:4dm1N",
 ]
+
+# retrieve data to provide score with models
+X_train, X_test, y_train, y_test = get_data()
+
+auth_header = Header(
+    default="Basic ",
+    description="""authorization header need to contain
+    credentials of user (username:password)""",
+)
+
+
+class Models(Enum):
+    """
+    Possible values for models
+    """
+
+    lgbm = "LGBMClassifier"
+    logistic = "LogisticRegression"
+    gaussian = "GaussianNB"
+    knn = "KNeighborsClassifier"
+    forest = "RandomForestClassifier"
+    lineardiscriminant = "LinearDiscriminantAnalysis"
+    svc = "SVC"
+    decisiontree = "DecisionTreeClassifier"
+
+
+class ModelsName(BaseModel):
+    """
+    Name of the model used by the request
+    """
+
+    model_name: Models
 
 
 def get_models_dict() -> dict:
@@ -52,25 +87,9 @@ def get_models_list() -> list:
     return list(get_models_dict().keys())
 
 
-@api.get("/", name="Check API")
-def index() -> dict:
+def check_authorization(authorization_header=auth_header):
     """
-    Return a dictionnary that indicate the api is running
-    """
-
-    return {"status": "running"}
-
-
-@api.get("/info", name="Get informations")
-def get_info(
-    authorization_header=Header(
-        default="Basic ",
-        description="""authorization header need to contain
-    credentials of user (username:password)""",
-    )
-) -> dict:
-    """
-    Return informations on the API
+    Function to check authorization-header and validate the credentials
     """
     if "Basic " not in authorization_header:
         raise HTTPException(
@@ -100,5 +119,30 @@ def get_info(
             detail="You do not have authorization to acces this ressource",
         )
 
+
+@api.get("/", name="Check API")
+def index() -> dict:
+    """
+    Return a dictionnary that indicate the api is running
+    """
+
+    return {"status": "running"}
+
+
+@api.get("/info", name="Get informations")
+def get_info(authorization_header=Depends(check_authorization)) -> dict:
+    """
+    Return informations on the API
+    """
+
     # Return models list
     return {"models": get_models_list()}
+
+
+@api.get("/score", name="Get performance score for a model")
+def score(authorization_header=Depends(check_authorization)) -> dict:
+    return {"score": 1}
+
+
+if __name__ == "__main__":
+    uvicorn.run("api_models:api", host="127.0.0.1", port=8000, reload=True)
